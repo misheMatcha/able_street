1846b2b73947d47dade348e5ab4c2d9314774300
misheMatcha <mishe.vong@gmail.com>
2021-04-24 14:15:53 -0700

    feat #1 - Add urlencoded and json to format for frontend use

--- server/src/server.js
+++ server/src/server.js
@@ -11,8 +11,11 @@ mongoose
 	.then(() => console.log('Successfully connected to MongoDB'))
 	.catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World'))
+// Express 4.16+ deprecated bodyParser, use express
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
+app.get('/', (req, res) => res.send('Hello World'))
 app.use('/api/users', users)
 
 const PORT = process.env.PORT || 5000
