10cbbcf7fc70f4e58d8ed5b88886acdf1217b9f2
misheMatcha <mishe.vong@gmail.com>
2021-04-24 14:02:37 -0700

    feat #1 - Connected mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import express from 'express'
+import mongoose from 'mongoose'
+import { mongoDB } from '../config/keys'
+
+const app = express()
+
+mongoose
+	.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Successfully connected to MongoDB'))
+	.catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
