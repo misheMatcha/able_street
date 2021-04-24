8b4ca5cba3196b553c520b5be9839585a412bcb5
misheMatcha <mishe.vong@gmail.com>
2021-04-24 16:26:54 -0700

    feat #1 - Add user mapping and adding for testing

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,6 +1,6 @@
 import { createUseStyles } from 'react-jss'
-import React, { useEffect, useState } from 'react'
-import { fetchUsers } from '../utils/user_api'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { addUser, fetchUsers } from '../utils/user_api'
 
 const useStyles = createUseStyles({
 	container: {
@@ -11,20 +11,46 @@ const useStyles = createUseStyles({
 const App = () => {
 	const classes = useStyles()
 	const [users, setUsers] = useState<any>()
+	const [name, setName] = useState('')
 
 	// Leaving useEffect and users for testing purposes only
 
 	useEffect(() => {
-		if (!users) {
-			fetchUsers()
-				.then(fetchedUsers => setUsers(fetchedUsers.data))
-				.catch(err => console.log(err))
-		}
-	})
+		fetchUsers()
+			.then(fetchedUsers => setUsers(fetchedUsers.data))
+			.catch(err => console.log(err))
+	}, [users])
+
+	const renderUsers = () => {
+		if (!users) return <div>no users</div>
+		return users.map((user: any, i: number) => {
+			return <div key={i}>{user.username}</div>
+		})
+	}
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setName(e.target.value)
+	}
+
+	const submitUser = () => {
+		addUser({
+			username: name
+		})
+	}
 
 	return (
 		<div className={classes.container}>
 			<div>App</div>
+			{renderUsers()}
+			<div>
+				<p>add user</p>
+				<input
+					placeholder='username'
+					value={name}
+					onChange={e => handleChange(e)}
+				/>
+				<button onClick={submitUser}>add user</button>
+			</div>
 		</div>
 	)
 }
