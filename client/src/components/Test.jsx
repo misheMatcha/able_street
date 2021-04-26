953c2875576e15befe4d2f880d067ea176fbc3a6
misheMatcha <mishe.vong@gmail.com>
2021-04-26 14:56:08 -0700

    feat #1 - tested adding users on frontend and persisiting in db

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { fetchUsers } from '../utils/userApi'
+import { addUser, fetchUsers } from '../utils/userApi'
 
 const Test = () => {
 	const [users, setUsers] = useState([])
+	const [username, setUsername] = useState('')
 
 	useEffect(() => {
-		if (users.length === 0)
+		if (users)
 			fetchUsers()
 				.then(userlist => setUsers(userlist.data))
 				.catch(err => console.log(err))
@@ -15,7 +16,30 @@ const Test = () => {
 		return users.map(({ username }, i) => <div key={i}>{username}</div>)
 	}
 
-	return <div>{renderUsers()}</div>
+	const handleInput = e => {
+		setUsername(e.target.value)
+	}
+
+	const handleSubmit = () => {
+		addUser({
+			username: username
+		})
+	}
+
+	return (
+		<div>
+			{renderUsers()}
+			<div>
+				<input
+					type='text'
+					placeholder='Username'
+					value={username}
+					onChange={e => handleInput(e)}
+				/>
+				<button onClick={handleSubmit}>add user</button>
+			</div>
+		</div>
+	)
 }
 
 export default Test
