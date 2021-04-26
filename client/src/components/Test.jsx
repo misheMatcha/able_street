8ec40bbab2feb3c4dd4f8e886ed2266061d46ccf
misheMatcha <mishe.vong@gmail.com>
2021-04-26 14:37:38 -0700

    feat #1 - install axios. create user api util, test fetching

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchUsers } from '../utils/userApi'
 
-const Test = () => <div>Testing local deploy</div>
+const Test = () => {
+	const [users, setUsers] = useState([])
+
+	useEffect(() => {
+		if (users.length === 0)
+			fetchUsers()
+				.then(userlist => setUsers(userlist.data))
+				.catch(err => console.log(err))
+	}, [users])
+
+	const renderUsers = () => {
+		return users.map(({ username }, i) => <div key={i}>{username}</div>)
+	}
+
+	return <div>{renderUsers()}</div>
+}
 
 export default Test
